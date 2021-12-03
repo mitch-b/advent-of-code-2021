428d170ed834afddd5d8122c1c8d377eff1b8db0
Mitchell Barry <mitch.barry@gmail.com>
2021-12-02 22:57:18 -0600

    Added Day 2 solution

--- AdventOfCode/Solutions/Year2021/Day02/Day02.cs
+++ AdventOfCode/Solutions/Year2021/Day02/Day02.cs
@@ -14,13 +14,57 @@ namespace AdventOfCode.Solutions.Year2021
         }
 
         protected override string SolvePartOne()
-        {
-            return null;
+        {            
+            var instructions = this.Input.SplitByNewline();
+            var horizontalPosition = 0;
+            var depth = 0;
+            foreach (var instruction in instructions)
+            {
+                var (action, value, _) = instruction.Split(' ');
+                var num = int.Parse(value);
+                switch (action) {
+                    case "forward":
+                        horizontalPosition += num;
+                        break;
+                    case "up":
+                        depth -= num;
+                        break;
+                    case "down":
+                        depth += num;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return (horizontalPosition * depth).ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var instructions = this.Input.SplitByNewline();
+            var horizontalPosition = 0;
+            var depth = 0;
+            var aim = 0;
+            foreach (var instruction in instructions)
+            {
+                var (action, value, _) = instruction.Split(' ');
+                var num = int.Parse(value);
+                switch (action) {
+                    case "forward":
+                        horizontalPosition += num;
+                        depth += (aim * num);
+                        break;
+                    case "up":
+                        aim -= num;
+                        break;
+                    case "down":
+                        aim += num;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return (horizontalPosition * depth).ToString();
         }
     }
 }
