2b504a8ba2c003a4f1592c36af343405a7305c35
Mitchell Barry <mitch.barry@gmail.com>
2021-12-06 22:29:13 -0600

    Added Day 6 solutions

--- AdventOfCode/Solutions/Year2021/Day06/Day06.cs
+++ AdventOfCode/Solutions/Year2021/Day06/Day06.cs
@@ -1,6 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2021
 {
@@ -10,17 +9,60 @@ namespace AdventOfCode.Solutions.Year2021
 
         public Day06() : base(06, 2021, "")
         {
-
+            
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var numberOfDays = 80;
+            var fishSpawnWindow = GenerateFishSpawnClassification(7);
+            AdvanceFishSpawnDays(fishSpawnWindow, numberOfDays);
+            return fishSpawnWindow.Sum(n => (long)n).ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var numberOfDays = 256;
+            var fishSpawnWindow = GenerateFishSpawnClassification(7);
+            AdvanceFishSpawnDays(fishSpawnWindow, numberOfDays);
+            return fishSpawnWindow.Sum(n => (long)n).ToString();
+        }
+
+        private long[] GenerateFishSpawnClassification(int spawnAge)
+        {
+            var fishSpawnWindow = new long[spawnAge+2];
+            var lanternfish = this.Input.Split(',')
+                .Select(f => int.Parse(f));
+            foreach (var fishAge in lanternfish)
+            {
+                fishSpawnWindow[fishAge]++;
+            }
+            return fishSpawnWindow;
+        }
+
+        private void AdvanceFishSpawnDays(long[] fishSpawnWindow, int days)
+        {
+            var spawnAge = fishSpawnWindow.Length - 2;
+            for (var i = 0; i < days; i++)
+            {
+                var newFishCount = fishSpawnWindow[spawnAge];
+                // shift # of fish left each day in array
+                for (var day = spawnAge + 1; day >= 0; day--)
+                {
+                    var priorWindowCount = newFishCount;
+                    newFishCount = fishSpawnWindow[day];
+                    fishSpawnWindow[day] = priorWindowCount;
+                }
+                // set all newly spawned fish around the spawnAge
+                fishSpawnWindow[spawnAge + 1] = newFishCount;
+                fishSpawnWindow[spawnAge - 1] += newFishCount;
+
+                if (this.Debug)
+                {
+                    Console.WriteLine($"After {(i+1).ToString("D2")} days: {fishSpawnWindow[0]},{fishSpawnWindow[1]},{fishSpawnWindow[2]},{fishSpawnWindow[3]},{fishSpawnWindow[4]},{fishSpawnWindow[5]},{fishSpawnWindow[6]},{fishSpawnWindow[7]},{fishSpawnWindow[8]}");
+                }
+            }
+            return;
         }
     }
 }
