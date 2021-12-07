3dcde19a202f9be7605b7a55e0fb45f4a41429f3
Mitchell Barry <mitch.barry@gmail.com>
2021-12-06 23:13:15 -0600

    Added Day 7 solutions

--- AdventOfCode/Solutions/Year2021/Day07/Day07.cs
+++ AdventOfCode/Solutions/Year2021/Day07/Day07.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2021
@@ -15,12 +16,40 @@ namespace AdventOfCode.Solutions.Year2021
 
         protected override string SolvePartOne()
         {
-            return null;
+            var positions = this.Input.Split(',').Select(p => int.Parse(p));
+            var maxPosition = positions.Max();
+            var minPosition = positions.Min();
+            var totalFuelCosts = new Dictionary<int, long>();
+            
+            for (var position = minPosition; position <= maxPosition; position++)
+            {
+                totalFuelCosts.Add(position, 0);
+                foreach (var crab in positions)
+                {
+                    totalFuelCosts[position] += Math.Abs(crab - position);
+                }
+            }
+
+            return totalFuelCosts.OrderBy(k => k.Value).First().Value.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var positions = this.Input.Split(',').Select(p => int.Parse(p));
+            var maxPosition = positions.Max();
+            var minPosition = positions.Min();
+            var totalFuelCosts = new Dictionary<int, long>();
+            
+            for (var position = minPosition; position <= maxPosition; position++)
+            {
+                totalFuelCosts.Add(position, 0);
+                foreach (var crab in positions)
+                {
+                    totalFuelCosts[position] += Enumerable.Range(1, Math.Abs(crab - position)).Sum();
+                }
+            }
+
+            return totalFuelCosts.OrderBy(k => k.Value).First().Value.ToString();
         }
     }
 }
